9ce58e47842d8cd6a06991d6ab65ff802034c837
Axel Velasquez <yoloax11@gmail.com>
2025-02-09 21:25:53 -0800

    updated the db access

--- backend/database/db.py
+++ backend/database/db.py
@@ -7,12 +7,11 @@ load_dotenv()
 supabase_url = os.getenv("SUPABASE_URL")
 supabase_key = os.getenv("SUPABASE_KEY")
 supabase_service_role_key = os.getenv("SUPABASE_SERVICE")
+supabase_bucket = os.getenv("SUPABASE_BUCKET")
+supabase_jwt_secret = os.getenv("SUPABASE_JWT_SECRET")
 
-if not all([supabase_url, supabase_key, supabase_service_role_key]):
-    raise ValueError("Please set the SUPABASE_URL and SUPABASE_KEY environment variables")
-
-if not supabase_service_role_key:
-    raise ValueError("Missing the admin role key")
+if not all([supabase_url, supabase_key, supabase_service_role_key, supabase_bucket]):
+    raise ValueError("Please set the supabase environment variables")
 
 supabase = create_client(supabase_url, supabase_key)
 supabase_admin = create_client(supabase_url, supabase_service_role_key)
