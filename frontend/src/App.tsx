259a516b1d4873158293605861f74c623bb1dd54
Axel Velasquez <yoloax11@gmail.com>
2025-02-10 19:37:07 -0800

    added the authorizatio provider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,18 +2,25 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css'
 import Home from './pages/Home';
 import Add from './pages/Add';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import { AuthProvider } from './context/AuthContext';
 
 function App() {
 
 
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/add" element={<Add />} />
-        </Routes>
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path = "/signup" element={<Signup />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/add" element={<Add />} />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
     </>
   )
 }
