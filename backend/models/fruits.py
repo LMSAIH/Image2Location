b137dd882f47a2c92e952af2c7eabd917cd4c3a2
Axel Velasquez <yoloax11@gmail.com>
2025-02-13 22:49:22 -0800

    deleted unused code

--- backend/models/fruits.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from typing import List
-
-class Fruit(BaseModel):
-    name: str
-    price: float
-
-class Fruits(BaseModel):
-    fruits: List[Fruit]  # Changed to lowercase to match usage
