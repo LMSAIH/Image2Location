46f982df2ef22b1a2170f4bdccf6a02e65b4a507
Axel Velasquez <yoloax11@gmail.com>
2025-02-13 22:49:46 -0800

    created model for the infolocation

---
+++ backend/models/infoLocation.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel
+from typing import List
+
+class LocationInfo(BaseModel):
+    Message: str
+    Best_Seasons: List[str]
+    Best_attractions: List[str]
+    Good_Hotels: List[str]
+    
