dc68daa3e1d15adeebbdaf9dda91478d85fb0f47
Axel Velasquez <yoloax11@gmail.com>
2025-02-16 14:29:42 -0800

    updated the login to get the proper functionality going

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,30 +1,32 @@
 import { FormEvent } from "react"
 import { useState } from 'react'
 import axios from "axios";
-
+import { useAuth } from "../context/AuthContext";
 
 export default function Login() {
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [error, setError] = useState<string | null>(null);
+    const { refreshAuth } = useAuth();
 
     const handleClick = async (e: FormEvent) => {
         e.preventDefault();
 
         try {
             const response = await axios.post('http://localhost:8000/login', {
-                email: email,  
+                email: email,
                 password: password
             }, {
-                withCredentials: true  
+                withCredentials: true
             });
 
-            if (response.status === 200) {
-                window.location.href = '/';  
-                setError(null);
-            }
-            
+            console.log(response.data)
+
+            await refreshAuth();
+            setError(null);
+            window.location.href = "/";
+
         } catch (err: any) {
             setError(err.response?.data?.detail || "An error occurred");
         }
