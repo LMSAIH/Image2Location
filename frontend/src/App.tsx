f2c22e93a49d50246aeca044af73c622f280769e
Axel Velasquez <yoloax11@gmail.com>
2025-02-16 14:28:20 -0800

    changes the auth provider location on the app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,23 +4,24 @@ import Home from './pages/Home';
 import Add from './pages/Add';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
-import { AuthProvider } from './context/AuthContext';
+import { useAuth } from './context/AuthContext';
 
 function App() {
 
+  const { user } = useAuth();
 
   return (
     <>
-      <AuthProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path = "/signup" element={<Signup />} />
-            <Route path="/" element={<Home />} />
-            <Route path="/add" element={<Add />} />
-          </Routes>
-        </BrowserRouter>
-      </AuthProvider>
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/" element={user === null ? <Login /> : <Home />} />
+          <Route path="/add" element={<Add />} />
+        </Routes>
+      </BrowserRouter>
+
     </>
   )
 }
