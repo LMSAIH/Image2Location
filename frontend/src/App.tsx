21f302173c0e397b8bca749f87bae94150e4cc69
Axel Velasquez <yoloax11@gmail.com>
2025-02-16 21:13:33 -0800

    Added the navbar along with the different required pages and components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css'
 import Home from './pages/Home';
-import Add from './pages/Add';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+import { Navbar } from './components/Navbar';
 import { useAuth } from './context/AuthContext';
+import LocationsList from './pages/List';
+import Location from './pages/Locations';
 
 function App() {
 
@@ -14,11 +16,14 @@ function App() {
     <>
 
       <BrowserRouter>
+        {user === null ? null : <Navbar />}
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/" element={user === null ? <Login /> : <Home />} />
-          <Route path="/add" element={<Add />} />
+          <Route path="/list" element={user === null ? <Login /> : <LocationsList />} />
+          <Route path="/location/:id" element={user === null ? <Login /> : <Location />} />
+
         </Routes>
       </BrowserRouter>
 
